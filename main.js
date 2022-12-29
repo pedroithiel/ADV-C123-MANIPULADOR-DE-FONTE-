2a7dd050e1b0d33789551b8a139ab94d508423e8
pedroithiel <101153331+pedroithiel@users.noreply.github.com>
2022-12-29 16:45:09 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,37 +1,46 @@
-noseX = 0;
-noseY = 0;
-leftWristX = 0;
-rightWristX = 0;
+var difference = 0;
+var leftWristX = 0;
+var rightWristX = 0;
+var texto;
 function setup() {
-    canvas = createCanvas(700,700)
-    canvas.center()
-    video = createCapture(VIDEO)
-    video.size(500,500)
+    canvas = createCanvas(700, 700);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(500, 500);
 
-    }
+}
 
-    function draw(){
-        background("#646566");
-        poseNet = ml5.poseNet(video, modeload)
-        poseNet.on("pose", gotPose)
-    }
-    function modeload() {
-        console.log("model load")
-    }
+function draw() {
+    texto = elemento("texto").value;
     
-    function gotPose(results) {
-        if (results.length > 0) {
-            console.log(results)
-            noseX = results[0].pose.nose.x;
-            noseY = results[0].pose.nose.y;
-            leftWristX = results[0].pose.leftWrist.x;
-            rightWristX = results[0].pose.leftWrist.x;
-        } else {
-            console.log("algo deu error")
-            //alert("ocorreu um error, você pode ter saido da visão da webCam")
-        }
-    }
+    background("#32a852");
+    poseNet = ml5.poseNet(video, modeload);
+    poseNet.on("pose", gotPose);
+    
+    fill("#0f0f0f");
+    
+    textSize(difference);
+    text(texto, leftWristX, rightWristX);
+    
+    //textSize(30);
+}
+function modeload() {
+    console.log("model load")
+}
 
-    function elemento(name) {
-        return documen.getElementById(name);
+function gotPose(results) {
+    if (results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        
+    } else {
+        console.log("algo deu error");
+        //alert("ocorreu um error, você pode ter saido da visão da webCam")
     }
+}
+
+function elemento(name) {
+    return document.getElementById(name);
+}
